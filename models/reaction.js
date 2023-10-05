2c5230dedd6a38581ca2bc362cc40c60ae0dbd9d
AngelaFig <angelafigueroa596@gmail.com>
2023-10-05 13:46:39 -0400

    added models/schema'

--- models/reaction.js
+++ models/reaction.js
@@ -3,6 +3,26 @@ const mongoose = require('mongoose');
 const reactionSchema = new mongoose.Schema({
     reactionId:{
         type:Schema.Types.ObjectId,
-        
+        default:()=>new mongoose.Types.ObjectId()
+    },
+    reactionBody:{
+        type:String,
+        required:true,
+        maxlength:280,
+    },
+    username:{
+        type:String,
+        required:true,
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+        // put getter here to format default date
     }
-})
+},{
+    toJSON:{
+        getters:true,
+    }
+}
+);
+module.exports= reactionSchema;
---
+++ models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:true,
+        required:true,
+        trimmed: true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        match:[/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,'Must use a vaild email address'],
+    },
+    thoughts:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'thought',
+        }
+    ],
+    friends:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'user',
+        }
+    ]
+},{
+    toJSON:{
+        getters:true,
+        virtuals:true,
+    }
+});
+
+userSchema.virtual('friendCount').get(function (){
+    return this.friends.length
+});
+
+const User = mongoose.model('user',userSchema);
+
+module.exports = User;
