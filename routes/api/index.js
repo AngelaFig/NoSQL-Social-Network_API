7e7c4cfe7aeb53beb721f13355f51243a8a33730
AngelaFig <angelafigueroa596@gmail.com>
2023-10-11 11:25:11 -0400

    added thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/users', userRoutes);
 //comment back in when done with thought controller and routes
-//router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
